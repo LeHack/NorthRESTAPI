e0f4d5be6926c2c99650b1171822e3aa3efe9480
Łukasz Hejnak (LeHack) <lehack-ghub@lehack.pl>
2016-12-03 10:41:22 +0100

    Added Updated and Delete REST endpoints for Shipper

--- src/main/java/northwind/rest/app/service/ShipperService.java
+++ src/main/java/northwind/rest/app/service/ShipperService.java
@@ -12,6 +12,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -58,5 +61,36 @@ public class ShipperService extends BaseService {
         dao.closeSession();
 
         return Response.created(new URI("/rest/shipper/"+s.getId())).build();
+    }
+
+    @POST
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateShipper(Shipper s ) throws URISyntaxException {
+        if(s == null){
+            return Response.status(400).entity("Please specify shipper details to update.").build();
+        }
+
+        Transaction t = dao.openSession().beginTransaction();
+        Shipper fromDb = dao.getById(s.getId());
+        fromDb.setCompanyName( s.getCompanyName() );
+        dao.update(fromDb);
+        t.commit();
+        dao.closeSession();
+
+        return Response.created(new URI("/rest/shipper/"+s.getId())).build();
+    }
+
+    @GET
+    @Path("/delete/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response dropShipper(@PathParam("id")Integer id) throws URISyntaxException {
+        Transaction t = dao.openSession().beginTransaction();
+        Shipper shipper = dao.getById(id);
+        dao.delete(shipper);
+        t.commit();
+        dao.closeSession();
+        return Response.status(200).entity("Shipper: " + id + " deleted from database.").build();
     }
 }
